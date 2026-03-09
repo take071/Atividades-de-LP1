571c54280a0b6503abac97ad811c96a17c0a7b20
Felipe <arthurfelipessousa@gmail.com>
2026-03-09 16:00:41 -0300

    Add C program demonstrating pointer usage

---
+++ arroizee.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+   
+   int x,d,c;
+   int *p = &x;
+ do{
+  printf("PONTEIROS  EM 'C'");
+   getchar();
+   system("clear");
+
+ printf("escolha se você quer o programa seja de forma automática ou manual...\n");
+ printf("- 1 para automático\n");
+ printf("- 2 para manual\n");
+ scanf("%d",&d);
+getchar();
+system("clear");
+
+switch(d) {
+ case 1:
+do {
+ printf("digite o valor de 'x', sem ser o número 19: \n");
+ scanf("%d",&x);
+ getchar(); system("clear");
+ if(x == 19) {
+ printf("19 não pode ser usado por ti\n");}
+ } while (x == 19);
+
+ printf("o valor de 'x' é: %d\n",x);
+ printf("o endereço de 'x' é: %d\n",&x);
+ 
+ *p =19;
+ printf("agora 'x' vale %d, por causa do '*p'\n",*p);
+
+break;
+
+ case 2:
+  printf("escolha um valor para ser posto no ponteiro: \n");
+  scanf("%d", &c);
+  getchar(); system("clear");
+  
+   printf("digite o valor de 'x': \n");
+   scanf("%d",&x);
+   getchar(); system("clear");
+  
+  int *p = &c;
+
+  printf("o valor dentro do ponteiro é %d\n",*p);
+  printf("o v*p = *p + c;alor de 'x' é: %d\n",x);
+  printf("o endereço de 'x' é: %d\n",&x);
+  break;
+  default:
+   printf(" escolha a opção certa\n");
+   getchar(); system("clear");
+ break;
+} 
+} while(d !=1 && d != 2);
+   return 0;
+}
